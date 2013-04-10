fc681543ef2ae2b06e62fc6d9f5c362ca7b90d0e
alex-vasilyev <alex.ammok@gmail.com>
2013-04-10 15:10:52 -0400

    commit

--- scrapper.app.js
+++ scrapper.app.js
@@ -1,3 +1,16 @@
+/**
+ *
+ * Info:
+ *      Simple scrapper application that shows title, description and clickable links to a user from given URL.
+ *
+ * How to use:
+ *      node scrapper.app.js siteName
+ * 
+ * Example:
+ *      node scrapper.app.js google.com
+ *
+**/
+
 var request             = require('request'),    
     jsdom               = require('jsdom'),
     fs                  = require('fs'),
@@ -9,11 +22,13 @@ var request             = require('request'),
 var myFunc = function( link, cb ){
     
     console.log( 'requesting page: '.c3 + link.c3 );
-    
+
+    // Step 1 - request to the page
     request({
             uri: link,
         }, function (err, response, body) {
         
+        // Handle response issues
             if ( err || response.statusCode !== 200 ) {
                 if ( !response ){
                     console.log( 'Ooops! page doesn`t exist or wrong URL format'.c2 )
@@ -25,6 +40,8 @@ var myFunc = function( link, cb ){
             } else {
 
                 console.log( 'response code: ' + response.statusCode )
+                
+                // Step 2 - invoking jsdom and jQuery
                 jsdom.env({
                     html: body,
                     src: [
@@ -38,6 +55,7 @@ var myFunc = function( link, cb ){
 
                             var $ = window.$;
 
+                            // Step 3, final part - parse content with jQuery selectors
                             console.log( '\nThis page is:\n'.c1 + $(body)[0]._ownerDocument._contentType )
                             console.log( '\nPage title: \n'.c1 + $('title').text().trim() );
                             console.log( $('head meta[name="description"]').attr('content') !== undefined ? '\nPage description: \n'.c1 + $('head meta[name="description"]').attr('content') + '\n' : '\nPage description: \n'.c1 + 'No description on the page\n'.c2);
@@ -57,4 +75,6 @@ var myFunc = function( link, cb ){
         }
     );
 };
+
+// checking if we are too lazy to add 'http://' in url parameter :)
 myFunc( argv._[0].slice(0,7) == 'http://' || argv._[0].slice(0,7) == 'https:/' ? argv._[0] : 'http://' + argv._[0], function(){process.exit(1);})
